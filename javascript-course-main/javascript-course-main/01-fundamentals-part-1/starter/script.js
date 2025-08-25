c54ee38690283659d6517e85eee7a6dc8ed1729b
dev-vergent <vergelsantiago0509@gmail.com>
2025-08-25 11:57:04 +0800

    js-course-1

---
+++ javascript-course-main/javascript-course-main/01-fundamentals-part-1/starter/script.js
@@ -0,0 +1,66 @@
+// JavaScript Fundamentals - Part 1
+// We'll write our code here!
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+// do not use var
+var job = "programmer";
+job = 'teacher';
+console.log(job);
+
+const country = 'Portugal';
+const language = 'Portugese';
+const population = 10;
+
+let ageNow = 25;
+age = 31;
+
+console.log('=== DATA TYPES ===');
+
+let weight = 45;
+console.log(typeof weight);
+
+let username = 'Max';
+console.log(typeof username);
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+console.log('=== EXERCISES ===');
+
+const myFirstName = 'Vergel';
+console.log(myFirstName, typeof myFirstName);
+let myAge = 20;
+console.log(myAge, typeof myAge);
+const favoriteNumber = 74;
+console.log(favoriteNumber, typeof favoriteNumber);
+const iLikeJavascript = true;
+console.log(iLikeJavascript, typeof iLikeJavascript);
+
+let score = 100;
+console.log(score);
+score = 150;
+console.log(score);
